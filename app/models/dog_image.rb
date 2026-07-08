2db20e9c5a4814b432e424e56a3fccdc2854713b
SethEPeters <speters32@academic.rrc.ca>
2026-07-08 14:06:50 -0500

    Add validations

--- app/models/dog_image.rb
+++ app/models/dog_image.rb
@@ -1,3 +1,5 @@
 class DogImage < ApplicationRecord
   has_many :dog_infos
+  validates :dog_image, presence: true
+  validates :dog_image, length: { maximum: 2000 }
 end
--- app/models/dog_info.rb
+++ app/models/dog_info.rb
@@ -1,3 +1,5 @@
 class DogInfo < ApplicationRecord
   belongs_to :dog_image
+  validates :name, presence: true
+  validates :breed, presence: true
 end
